8c1e9bd3462e15c2452f20dfd79ca0083f7a52d5
idk <120068827+ConeDragon@users.noreply.github.com>
2025-03-20 21:20:24 -0400

    flexibility

--- translate.py
+++ translate.py
@@ -1,9 +1,9 @@
 import requests
 import json
 
-def translate_text(text, target_language):
+def translate_text(text, source_language="auto", target_language="en"):
     text = text.replace("|"," ")
-    url = "https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=" + target_language + "&dt=t&q=" + text
+    url = "https://translate.googleapis.com/translate_a/single?client=gtx&sl=" + source_language + "&tl=" + target_language + "&dt=t&q=" + text
     response = requests.get(url)
     response_text = json.loads(response.text)
     translated_text = response_text[0][0][0]
