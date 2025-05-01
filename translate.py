896ed35c3f4a037be57d4ee164a93798d5d6de13
idk <120068827+ConeDragon@users.noreply.github.com>
2025-05-01 14:41:49 -0400

    what in the escape

--- translate.py
+++ translate.py
@@ -1,9 +1,10 @@
 import requests
 import json
+import urllib.parse
 
 def translate_text(text, source_language="auto", target_language="en"):
     text = text.replace("|"," ")
-    url = "https://translate.googleapis.com/translate_a/single?client=gtx&sl=" + source_language + "&tl=" + target_language + "&dt=t&q=" + text
+    url = "https://translate.googleapis.com/translate_a/single?client=gtx&sl=" + source_language + "&tl=" + target_language + "&dt=t&q=" + urllib.parse.quote(text, safe='')
     response = requests.get(url)
     response_text = json.loads(response.text)
     translated_text = response_text[0][0][0]
